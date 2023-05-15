f3c33c76f83283884d6c45fd8ab9f5a2ae355926
KatOli4ka <KatOli4ka87@mail.ru>
2023-05-15 20:53:05 +0500

    проверка 2

---
+++ src/main/java/pro/sky/diplom/entity/Avatar.java
@@ -0,0 +1,29 @@
+package pro.sky.diplom.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+/**
+ * Класс сущности "Avatar"
+ *
+ * @author Одокиенко Екатерина
+ */
+@Entity
+@Data
+@Table(name = "avatar")
+public class Avatar {
+    /**
+     * поле - айди аватара
+     */
+    @Id
+    @Column(name = "avatar_id")
+    private String id;
+    /**
+     * поле - тип дата-данных аватара
+     */
+    @Column(name = "image")
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] data;
+}
